c7a5ed1f394f01f03679a730d270e1f6eb163afb
Swap-nil-2003 <swapnil.aichbhaumik.cse25@heritageit.edu.in>
2024-08-04 22:51:03 +0530

    new changes

---
+++ data/index.ts
@@ -0,0 +1,154 @@
+export const navItems = [
+  { name: "About", link: "#about" },
+  { name: "Projects", link: "#projects" },
+  { name: "Experience", link: "#experience" },
+  { name: "Contact", link: "#contact" },
+];
+
+export const gridItems = [
+  {
+    id: 1,
+    title: "I prioritize client collaboration, fostering open communication",
+    description: "",
+    className: "lg:col-span-3 md:col-span-6 md:row-span-4 lg:min-h-[60vh]",
+    imgClassName: "w-full h-full",
+    titleClassName: "justify-end",
+    img: "/b1.svg",
+    spareImg: "",
+  },
+  {
+    id: 2,
+    title: "Reach out via the contacts given below",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+    imgClassName: "",
+    titleClassName: "justify-start",
+    img: "",
+    spareImg: "",
+  },
+  {
+    id: 3,
+    title: "My tech stack",
+    description: "I constantly try to improve",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+    imgClassName: "",
+    titleClassName: "justify-center",
+    img: "",
+    spareImg: "",
+  },
+  {
+    id: 4,
+    title: "Tech enthusiast with a passion for development.",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+    imgClassName: "",
+    titleClassName: "justify-start",
+    img: "/grid.svg",
+    spareImg: "/b4.svg",
+  },
+
+  {
+    id: 5,
+    title: "Currently working with DevOps tools for Product Engineering",
+    description: "The Inside Scoop",
+    className: "md:col-span-3 md:row-span-2",
+    imgClassName: "absolute right-0 bottom-0 md:w-96 w-60",
+    titleClassName: "justify-center md:justify-start lg:justify-center",
+    img: "/b5.svg",
+    spareImg: "/grid.svg",
+  },
+  {
+    id: 6,
+    title: "Drop an email at",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+    imgClassName: "",
+    titleClassName: "justify-center md:max-w-full max-w-60 text-center",
+    img: "",
+    spareImg: "",
+  },
+];
+
+export const projects = [
+  {
+    id: 1,
+    title: "YouTube Clone",
+    des: "A new look YouTube Application with enhanced user experience",
+    img: "/youtube.png",
+    iconLists: ["/re.svg", "/rapid.jpg", "/js.jpg", "/mui.jpg"],
+    link: "https://github.com/Swap-nil-2003/Youtube_Clone-ReactApp03",
+  },
+  {
+    id: 2,
+    title: "Crypto'ed - Crypto and Exchanges tracker",
+    des: "Crypto Tracker using ReactJS, Chakra UI, Chart.js and APIs",
+    img: "/cryptoed.png",
+    iconLists: ["/re.svg", "/chakra.jpg", "/js.jpg"],
+    link: "https://github.com/Swap-nil-2003/Crypto-ed-ReactApp",
+  },
+  {
+    id: 3,
+    title: "Modern Banking Website",
+    des: "A ReactJS based application for a banking website",
+    img: "/bank.png",
+    iconLists: ["/re.svg", "/tail.svg", "/js.jpg"],
+    link: "https://github.com/Swap-nil-2003/Business-Application",
+  },
+  {
+    id: 4,
+    title: "ShopperHub",
+    des: "An E-Commerce platform built using Next.js 14,Sanity.io,ShadCN UI,Tailwind CSS and Stripe",
+    img: "/shopper.png",
+    iconLists: [
+      "/next.svg",
+      "/tail.svg",
+      "/js.jpg",
+      "/sanity.jpg",
+      "/shadcn.jpg",
+    ],
+    link: "https://github.com/Swap-nil-2003/ShopperHub-Next14",
+  },
+];
+export const workExperience = [
+  {
+    id: 1,
+    title: "Web Developer Intern, TheCyberDelta",
+    desc: "Assisted in the development of CMS using Joomla and a web-based platform using React.js, enhancing interactivity",
+    className: "md:col-span-2",
+    thumbnail: "/exp1.svg",
+  },
+  {
+    id: 2,
+    title: "Product Engineering Intern, SETV Global",
+    desc: "Working with DevOps methodologies integrating frontend and backend for developing scalable products, UI/UX Design",
+    className: "md:col-span-2",
+    thumbnail: "/exp4.svg",
+  },
+  {
+    id: 3,
+    title: "Board Member, ACM HITK Student Chapter",
+    desc: "Served as Joint Secretary and Treasurer at the Association for Computing Machinery in college",
+    className: "md:col-span-1",
+    thumbnail: "/exp2.svg",
+  },
+  {
+    id: 4,
+    title: "Student Coordinator",
+    desc: "Served as Student Coordinator at Heritage Debsoc, Convened Heritage Parliamentary Debate 2023&2024",
+    className: "md:col-span-1",
+    thumbnail: "/exp3.svg",
+  },
+];
+
+export const socialMedia = [
+  {
+    id: 1,
+    img: "/git.svg",
+    link: "https://github.com/Swap-nil-2003",
+  },
+  {
+    id: 3,
+    img: "/link.svg",
+    link: "https://www.linkedin.com/in/swapnil-aichbhaumik-28a980224/",
+  },
+];
